4fd5d918097839bf308a8bb3fdec2727cf42874c
yus-torres <65917571+yus-torres@users.noreply.github.com>
2020-05-26 22:13:46 +0800

    Updates for today

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -25,44 +25,6 @@ $(document).ready(function(){
     });
 });
 
-// Smooth Scroll
-// Select all links with hashes
-$('a[href*="#"]')
-// Remove links that don't actually link to anything
-.not('[href="#"]')
-.not('[href="#0"]')
-.click(function(event) {
-	// On-page links
-	if (
-		location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-		&& 
-		location.hostname == this.hostname
-	) {
-		// Figure out element to scroll to
-		var target = $(this.hash);
-		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-		// Does a scroll target exist?
-		if (target.length) {
-			// Only prevent default if animation is actually gonna happen
-			event.preventDefault();
-			$('html, body').animate({
-				scrollTop: target.offset().top - 80
-			}, 1000, function() {
-				// Callback after animation
-				// Must change focus!
-				var $target = $(target);
-				$target.focus();
-				if ($target.is(":focus")) { // Checking if the target was focused
-					return false;
-				} else {
-					$target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-					$target.focus(); // Set focus again
-				};
-			});
-		}
-	}
-});
-
 // Slide Menu
 smoothScroll.init();
 $(document).ready(function() {
@@ -101,11 +63,3 @@ $('.btn-more').click(function() {
 		return old=='Read More' ?  'Read Less' : 'Read More';
 	});
 });
-
-// ScrollReveal
-ScrollReveal({ distance: '60px', delay: '200', duration: 1000, });
-ScrollReveal().reveal('.sr-in');
-ScrollReveal().reveal('.sr-up', { origin: 'bottom' });
-ScrollReveal().reveal('.sr-down', { origin: 'top' });
-ScrollReveal().reveal('.sr-left', { origin: 'right' });
-ScrollReveal().reveal('.sr-right', { origin: 'left' });
